27b93c36f223742faa9f05a6f1c48257e84bf9c5
xmori <xmori@example.com>
2025-03-04 14:10:04 +0800

    rewrite analyse_compile.rb

--- tools/analyse_compile.rb
+++ tools/analyse_compile.rb
@@ -1,23 +1,33 @@
-path = ARGV[0]
-pp ARGV
-pp $*
-unless test 'f', path
-  raise "** NOENT #{path}"
+compdb_path = ARGV[0]
+
+unless test 'f', compdb_path
+  raise "[E] invalid compile_commands.json path #{compdb_path}"
 end
 
 require 'json'
 
-js = JSON.load_file path
+json = JSON.load_file compdb_path
 
-js.map {
-  x = _1['file']
+# [
+#   {
+#     "arguments": [], // arguments to gcc
+#     "directory": [], // working directory where the compiling occurred
+#     "file":"",       // the file being compiled
+#     "output": ""     // the output file
+#   }
+# ]
+# 
+# What we did here is to group "file" by their directory names
+# to see which directory owns the most files.
+
+json.map {
+  _1['file']             # extract files
 }.group_by {
-  File.dirname _1
-}.sort_by { |dir, child|
-  child.size
-}.reverse.map { |dir,child|
-  "#{dir} #{child.size}"
+  File.dirname _1        # group by directoriy names
+}.sort_by { |dir, files|
+  files.size             # sort by number of files
+}.reverse.map { |dir, files|
+  "#{dir} #{files.size}" # directories that have most files come first
 }.join("\n").tap{
-  puts _1
-  # File.write "compile_result", _1
+  puts _1                # output the result  
 }
