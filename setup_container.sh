0cb0d43f18c694631fe01b0bc4827aab574a35d2
xmori <xmori@example.com>
2025-03-06 14:00:17 +0800

    improve setup_container.sh

--- setup_container.sh
+++ setup_container.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
 
-if which podman; then
+if which podman > /dev/null; then
   hash=`podman run --arch=i386 --personality=LINUX32 --detach --tty \
-    --mount=type=bind,src=`pwd`,dst=/root/build_linux_2.6.11 \
+    --mount=type=bind,src=$(pwd),dst=/root/build_linux_2.6.11 \
     docker.io/library/debian:bookworm`
   
   podman exec $hash apt update
-  podman exec $hash apt install bear make
+  podman exec $hash apt install -y bear make
 
+  echo -e "\n[*] done, the newly created container is $hash"  
+else
+  echo "you need to install podman first."
+  exit 1
 fi
